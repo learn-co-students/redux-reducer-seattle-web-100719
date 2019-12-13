6445c918dc7a4f672f7ceda01632fa1000da8d69
Matt Gahrns <matthewgahrns@hotmail.com>
2019-12-12 16:11:19 -0800

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,29 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      let friend = {
+        name: action.friend.name,
+        hometown: action.friend.hometown,
+        id: action.friend.id
+      };
+      state = {
+        friends: [...state.friends, friend]
+      };
+      return state;
+    case "REMOVE_FRIEND":
+      let i;
+      let arr = state.friends;
+      state.friends.map((friend, index) => {
+        if (friend.id === action.id) {
+          i = index;
+          arr.splice(i, 1);
+        }
+      });
+      const res = {
+        friends: arr
+      };
+      return res;
+    default:
+      return state;
+  }
 }
